fd59b191defa8bf2fe791d15fd09b5eb71582ba9
AG <Anthony459067@gmail.com>
2023-07-26 07:20:06 -0400

    Add table displaying items

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -4,9 +4,11 @@ const asyncHandler = require("express-async-handler");
 exports.index = asyncHandler(async (req, res, next) => {
     // Get details of books, book instances, authors and genre counts (in parallel)
     const numItems = await Item.countDocuments({}).exec();
+    const allItems = await Item.find({}).exec();
   
     res.render("index", {
       title: "Inventory",
       item_count: numItems,
+      item_list: allItems
     });
   });
