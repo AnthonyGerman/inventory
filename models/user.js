5c392b4e98717d42aebdd7c3860ed11c8eb496cb
AG <Anthony459067@gmail.com>
2023-07-26 21:14:41 -0400

    attempt to fix log in issues

--- models/user.js
+++ models/user.js
@@ -4,6 +4,13 @@ mongoose.set('strictQuery', false);
 const passportLocalMongoose = require('passport-local-mongoose');
 require('dotenv').config();
 
+main().catch((err) => console.log(err));
+async function main() {
+    await mongoose.connect(process.env.mongoLink,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+}
 
 // Create Model
 const Schema = mongoose.Schema;
